8b5555240f1da6e5cc2ba927b494b526f6ecc95f
Sakii.codes <sakshiv2103@gmail.com>
2025-02-06 19:46:08 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = ["Backend Developer","ML Engineer","App Developer"];
+  const toRotate = ["Full-Stack Developer","Backend Developer","ML Engineer"];
   const period = 2000;
 
   useEffect(() => {
